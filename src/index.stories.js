a518d330e625ad3ffd7d66c5cd5bad07e324ff6a
muryoh <muryoh@gmail.com>
2021-03-08 15:21:09 +0000

    First commit

---
+++ src/index.stories.js
@@ -0,0 +1,11 @@
+import "./index.js";
+import "vanilla-colorful";
+
+export default {
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export const story1 = () => `<my-counter></my-counter>`;
+export const story2 = () => `<rgba-color-picker></rgba-color-picker>`;
